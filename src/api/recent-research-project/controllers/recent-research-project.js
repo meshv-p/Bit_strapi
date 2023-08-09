671403cfd7ac155a2713e826e16a2e4ced38e78b
Meshv Patel <meshv1823@gmail.com>
2023-08-09 11:11:05 +0530

    Init strapi

---
+++ src/api/recent-research-project/controllers/recent-research-project.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * recent-research-project controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::recent-research-project.recent-research-project');
---
+++ src/api/recent-research-project/routes/recent-research-project.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * recent-research-project router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::recent-research-project.recent-research-project');
